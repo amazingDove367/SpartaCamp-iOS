bf30269ed38fd722a6fd2ae9d8c4341ced46736b
Hakyung Sohn <hortenssiaa@gmail.com>
2025-02-27 15:10:15 +0900

    commit [mini#3] MemoApp

--- [mini#3] MemoApp/[mini#3] MemoApp/MemoCell.swift
+++ [mini#3] MemoApp/[mini#3] MemoApp/MemoCell.swift
@@ -8,16 +8,36 @@
 import UIKit
 
 class MemoCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    static let identifier = "MemoCell"
+    
+    private let memoLable: UILabel = {
+        let lable = UILabel()
+        lable.textColor = .label
+        lable.font = .systemFont(ofSize: 30, weight: .bold)
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        return lable
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureAutoLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func configureAutoLayout() {
+        // viewcontroll 와 같이 view 가 아님! -> contentView
+        contentView.addSubview(memoLable)
+        NSLayoutConstraint.activate([
+            memoLable.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            memoLable.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+        ])
+    }
+    
+    func configureCell(memoData: String) {
+        memoLable.text = memoData
+    }
 }
