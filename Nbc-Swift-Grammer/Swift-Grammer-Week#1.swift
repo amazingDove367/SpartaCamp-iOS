9ebc22c0ed27a3417990dfbc9736c0a75fc79451
Hakyung Sohn <hortenssiaa@gmail.com>
2025-03-10 18:13:56 +0900

    Nbc Swift Grammer Week #1

--- Nbc-Swift-Grammer/Swift-Grammer-Week#1.swift
+++ Nbc-Swift-Grammer/Swift-Grammer-Week#1.swift
@@ -1,14 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>BaseFile</key>
-	<string>Chapters/Chapter1.playgroundchapter/Pages/Template.playgroundpage/main.swift</string>
-	<key>Diffs</key>
-	<array>
-		<dict>
-			<key>ModifiedContent</key>
-			<string>// Swift grammer
+// Swift grammer
 
 // Tuple
 // - 데이터 타입의 값들을 그룹화 하여 '하나의 타입'처럼 사용
@@ -23,7 +13,7 @@ let student: [(name: String, age: Int, major: String)] = [
 print(student[1])
 
 
-// Switch 
+// Switch
 // ⭐️ fallthrough
 // 1
 var score = 50
@@ -43,15 +33,15 @@ case 50...:
 case 50:
     print("딱 50점이시네요?")
 default:
-    print("공부를 더 하세요!") 
+    print("공부를 더 하세요!")
 }
 
 
 // for
 // 1. 숫자 출력: 1부터 10까지의 숫자를 출력하는 반복문을 개발하세요.
 //   현재의 값이 8 이상이라면 guard를 사용하여 반복문에서 탈출해보세요.
-for i in 1...10 {    
-    guard i &lt; 8 else { break }
+for i in 1...10 {
+    guard i < 8 else { break }
     print(i)
 }
 
@@ -72,7 +62,7 @@ for i in 1...20 {
 // while
 // 1. 숫자 감소: 10부터 1까지 숫자를 감소하며 출력하세요.
 var i1 = 10
-while i1 &gt; 0 {
+while i1 > 0 {
     print(i1)
     i1 -= 1
 }
@@ -80,7 +70,7 @@ while i1 &gt; 0 {
 // 2. 1부터 5까지 합 구하기: 1부터 5까지의 합을 while 문으로 계산하세요.
 var i = 1
 var sum = 0
-while i &lt;= 5 {
+while i <= 5 {
     sum += i
     i += 1
 }
@@ -123,8 +113,8 @@ for i in intArr {
 // - 타입 명시!
 
 // 1. Set 연습
-var stringSet: Set&lt;String&gt; = []
-var intSet: Set&lt;Int&gt; = Set()
+var stringSet: Set<String> = []
+var intSet: Set<Int> = Set()
 
 print(stringSet.contains("a"))
 
@@ -147,9 +137,9 @@ print(setSet)
 
 
 
-// Dictionary 
+// Dictionary
 // - 순서 x
-// - key:value 
+// - key:value
 // - key 중복 x
 
 // 1. Dictionary 실습하기 : 아래의 코드를 만들어주세요.
@@ -170,43 +160,43 @@ print(dictionary.count)
 // Closure (aka. 이름없는 함수)
 /*
  1. data type 으로 사용하는 경우
-   - 파라미터 타입 없는 경우; () -&gt; Void
+   - 파라미터 타입 없는 경우; () -> Void
    - Optional 타입 사용 가능
-     - (() -&gt; Void)?
-     let optionalClosure: ((Int) -&gt; Int)?
+     - (() -> Void)?
+     let optionalClosure: ((Int) -> Int)?
  */
 
 // 1. 파라미터는 튜플타입 (Int, String), 반환타입은 없음!
-let closure: (Int, String) -&gt; Void = { intValue, stringValue in
+let closure: (Int, String) -> Void = { intValue, stringValue in
     print(intValue)
-    print(stringValue)    
+    print(stringValue)
 }
 closure(40, "이거 읽어봐")
 
 
 // 2. 파라미터의 이름을 생략하면 구현부에서 $0로 사용할 수 있습니다.
-let closure2: (Int) -&gt; Void = {
+let closure2: (Int) -> Void = {
     print($0)
 }
 closure2(1004)
 
 
 // 3. 파라미터 없는 클로저
-let closure3: () -&gt; Void = {
+let closure3: () -> Void = {
     print("Hello!")
 }
 closure3()
 
 
 // 4. 파라미터 Int, output Int
-let closure4: (Int) -&gt; Int = { value in 
+let closure4: (Int) -> Int = { value in
     return value * 2
 }
 print(closure4(10))
 
 
-// 5. 튜플타입의 파라미터로, 파라미터 이름생략 &amp; $0 사용하기
-let closure5: (String, Int) -&gt; Int = {
+// 5. 튜플타입의 파라미터로, 파라미터 이름생략 & $0 사용하기
+let closure5: (String, Int) -> Int = {
     return $0.count + $1
 }
 print(closure5("200", 3))
@@ -214,26 +204,26 @@ print(closure5("200", 3))
 
 
 // 6. Optional Closure
-let optionalClosure: ((Int) -&gt; Int)? = { value in
-    return value * 2    
+let optionalClosure: ((Int) -> Int)? = { value in
+    return value * 2
 }
 print(optionalClosure!(40))
 
 
-// 7. 파라미터가 없을 때는 in을 사용하면 안됩니다. 
-let emptyParameterClosure: () -&gt; Void = {
+// 7. 파라미터가 없을 때는 in을 사용하면 안됩니다.
+let emptyParameterClosure: () -> Void = {
     print("Hello Im Emmpty")
 }
 emptyParameterClosure()
 
 
 
-// 8. 
+// 8.
 // 1️⃣  아래의 조건에 맞는 기본 클로저를 만들어보세요.
 // - minusClosure 상수의 타입은 (Int, Int) → Int 입니다.
 // - 클로저 구현부는 두개의 Int값을 받아서 뺀 뒤 반환합니다.
 // - minusClosure 상수를 호출해보세요.
-let minusClosure: (Int, Int) -&gt; Int = { v1, v2 in
+let minusClosure: (Int, Int) -> Int = { v1, v2 in
     return v1 - v2
 }
 print(minusClosure(10, 20))
@@ -241,7 +231,7 @@ print(minusClosure(10, 20))
 // 2️⃣  1번에서 만든 클로저에 파라미터 이름을 생략해보세요.
 
 
-let minusClosure2: (Int, Int) -&gt; Int = {
+let minusClosure2: (Int, Int) -> Int = {
     return $0 - $1
 }
 print(minusClosure2(50, 10))
@@ -249,8 +239,8 @@ print(minusClosure2(50, 10))
 
 
 // 9. Trailing Closire
-// : 경량화 방법 중 하나. 
-func trailingCloure(key: String, closure: () -&gt; Void) {
+// : 경량화 방법 중 하나.
+func trailingCloure(key: String, closure: () -> Void) {
     print(key)
     closure() // ⭐️
 }
@@ -261,24 +251,10 @@ trailingCloure(key: "trailing Closure Test1", closure: {
 })
 
 // (2) 클로저 구현이 길어지는 경우, 클로저함수호출 외부에 작성하여, 가독성 높이기
-trailingCloure(key: "trailing Closure Test2") { 
+trailingCloure(key: "trailing Closure Test2") {
     print("Second trailing Closure")
 }
 
 
 // 자료구조 : Stack, Queue
-// &gt;&gt; Array, Set 등의 메모리를 더 효율적으로 활용하기 위해
-</string>
-			<key>ModifiedRange</key>
-			<string>{0, 4236}</string>
-			<key>OriginalContent</key>
-			<string>
-</string>
-			<key>OriginalRange</key>
-			<string>{0, 1}</string>
-		</dict>
-	</array>
-	<key>FormatVersion</key>
-	<integer>2</integer>
-</dict>
-</plist>
+// >> Array, Set 등의 메모리를 더 효율적으로 활용하기 위해
