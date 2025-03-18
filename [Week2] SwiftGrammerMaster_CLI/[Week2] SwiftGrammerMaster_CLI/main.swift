0b5c4186d47a1e9e9cb261dc14c895f82b96a842
Hakyung Sohn <hortenssiaa@gmail.com>
2025-03-18 20:15:07 +0900

    :sparkles: feat: 필수 구현 1
    1. 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저를 설계해주세요.
       - 클로저 내부에서는 두 정수를 더한 후, ''두 수의 합은 {합계} 입니다''라는 문자열을 반환합니다.
    2. 이 클로저를 상수 sum에 저장하고, 정확한 타입을 명시해주세요.
    3. sum을 호출하는 코드를 작성해주세요. (파라미터로 전달하는 값은 임의로 선택)

--- [Week2] SwiftGrammerMaster_CLI/[Week2] SwiftGrammerMaster_CLI/main.swift
+++ [Week2] SwiftGrammerMaster_CLI/[Week2] SwiftGrammerMaster_CLI/main.swift
@@ -7,3 +7,10 @@
 
 import Foundation
 
+// 필수 문제 1
+// [1.1] 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저를 설계해주세요.
+var sum: (Int, Int) -> String = { (v1: Int, v2: Int) in
+    return "두 수의 합은 \(v1 + v2) 입니다"
+}
+
+print(sum(4, 1000))
