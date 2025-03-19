9c47aca51c09ca2ad0a59fe966a121b5343d1ae1
Hakyung Sohn <hortenssiaa@gmail.com>
2025-03-19 18:16:45 +0900

    :sparkles: feat : 필수 구현 3.2 String 배열의 짝수번째 요소를 제거해서 반환하는 함수 b 를 작성해주세요.

--- [Week2] SwiftGrammerMaster_CLI/[Week2] SwiftGrammerMaster_CLI/Problems/Problem3.swift
+++ [Week2] SwiftGrammerMaster_CLI/[Week2] SwiftGrammerMaster_CLI/Problems/Problem3.swift
@@ -31,3 +31,29 @@ func runA() {
     print(a(array))
 }
 
+
+
+/// [3.2] String 배열의 짝수번째 요소를 제거해서 반환하는 함수 b 를 작성해주세요.
+
+func b(_ arrayString: String) -> [String] {
+    
+    // String to [String]
+    let array = arrayString.filter { $0.isLetter || $0 == "," }
+        .split(separator: ",")
+        .compactMap { String($0) }
+    
+    
+    let evenArray = array.enumerated()
+        .filter { $0.offset.isMultiple(of: 2) }
+        .map { $0.element }
+    
+    return evenArray
+}
+
+func runB() {
+    let array = readInput(prompt: "배열 입력:") // ex) [”가”, “나”, “다”, “라”, “마”]
+    print(b(array))
+}
+
+
+
