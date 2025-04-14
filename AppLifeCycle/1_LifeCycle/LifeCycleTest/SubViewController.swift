4bb9c1cfde198d0a6d912cfd8f395455b9c8f411
Hakyung Sohn <hortenssiaa@gmail.com>
2025-04-14 18:03:13 +0900

    :tada: Feat: UIViewController, View LifeCycle

---
+++ AppLifeCycle/1_LifeCycle/LifeCycleTest/SubViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SubViewController.swift
+//  LifeCycleTest
+//
+//  Created by kingj on 4/14/25.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+        print("\nSubViewController viewDidLoad\n")
+    }
+}
---
+++ AppLifeCycle/2_LifeCyclePractice/2_LifeCyclePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_LifeCyclePractice
+//
+//  Created by kingj on 4/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
